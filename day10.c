af014063e9a076e3e4ed798ddc8a890aac953c42
Shubhansh-source <Shubhansh.34711@stu.upes.ac.in>
2026-09-23 00:28:35 +0530

    day10

---
+++ day10.c
@@ -0,0 +1,37 @@
+include <stdio.h>
+
+int main() {
+    int day;
+
+    printf("Enter a number (1-7): ");
+    scanf("%d", &day);
+
+    switch (day) {
+        case 1:
+            printf("\nMonday\n");
+            break;
+        case 2:
+            printf("\nTuesday\n");
+            break;
+        case 3:
+            printf("\nWednesday\n");
+            break;
+        case 4:
+            printf("\nThursday\n");
+            break;
+        case 5:
+            printf("\nFriday\n");
+            break;
+        case 6:
+            printf("\nSaturday\n");
+            break;
+        case 7:
+            printf("\nSunday\n");
+            break;
+        default:
+            printf("\nError: Invalid input! Please enter a number between 1 and 7.\n");
+            break;
+    }
+
+    return 0;
+}
